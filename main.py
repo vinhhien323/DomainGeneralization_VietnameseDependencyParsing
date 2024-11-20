437f0679e0e7de08e1a629b85ebd07e0191b3914
Hien <huynhdangvinhhien232@gmail.com>
2024-11-20 13:53:06 +0700

    Add default type for args

--- main.py
+++ main.py
@@ -6,35 +6,35 @@ from model import Biaffine, Dependency_Parsing
 if __name__ == "__main__":
     config = argparse.ArgumentParser(description='Configuration for Biaffine Dependency Parsing')
     # Train set arguments
-    config.add_argument('--train_dir', action='store', default=None)
-    config.add_argument('--train_use_domain', action='store', default=False)
-    config.add_argument('--train_use_folder', action='store', default=False)
+    config.add_argument('--train_dir', action='store', default='', type=str)
+    config.add_argument('--train_use_domain', action='store', default=False, type=bool)
+    config.add_argument('--train_use_folder', action='store', default=False, type=bool)
 
     # Dev set arguments
-    config.add_argument('--dev_dir', action='store', default=None)
-    config.add_argument('--dev_use_domain', action='store', default=False)
-    config.add_argument('--dev_use_folder', action='store', default=False)
+    config.add_argument('--dev_dir', action='store', default='', type=str)
+    config.add_argument('--dev_use_domain', action='store', default=False, type=bool)
+    config.add_argument('--dev_use_folder', action='store', default=False, type=bool)
 
-    #     # Test set arguments
-    config.add_argument('--test_dir', action='store', default=None)
-    config.add_argument('--test_use_domain', action='store', default=False)
-    config.add_argument('--test_use_folder', action='store', default=False)
+    # Test set arguments
+    config.add_argument('--test_dir', action='store', default='', type=str)
+    config.add_argument('--test_use_domain', action='store', default=False, type=bool)
+    config.add_argument('--test_use_folder', action='store', default=False, type=bool)
 
     # Embedding arguments
-    config.add_argument('--embedding_type', action='store', default='roberta')
-    config.add_argument('--embedding_name', action='store', default=None)
+    config.add_argument('--embedding_type', action='store', default='roberta', type=str)
+    config.add_argument('--embedding_name', action='store', default='', type=str)
 
     # Model parameters
-    config.add_argument('--arc_mlp', action='store', default=500)
-    config.add_argument('--label_mlp', action='store', default=100)
-    config.add_argument('--drop_out', action='store', default=0.33)
-    config.add_argument('--batch_size', action='store', default=32)
-    config.add_argument('--learning_rate', action='store', default=5e-5)
-    config.add_argument('--lr_rate', action='store', default=10)
-    config.add_argument('--n_epochs', action='store', default=5)
+    config.add_argument('--arc_mlp', action='store', default=500, type=int)
+    config.add_argument('--label_mlp', action='store', default=100, type=int)
+    config.add_argument('--drop_out', action='store', default=0.33, type=float)
+    config.add_argument('--batch_size', action='store', default=32, type=int)
+    config.add_argument('--learning_rate', action='store', default=5e-5, type=float)
+    config.add_argument('--lr_rate', action='store', default=10, type=float)
+    config.add_argument('--n_epochs', action='store', default=5, type=int)
 
     # Device arguments
-    config.add_argument('--device', action='store', default='cpu')
+    config.add_argument('--device', action='store', default='cpu', type=str)
 
     args = config.parse_args()
     torch.set_default_device(args.device)
